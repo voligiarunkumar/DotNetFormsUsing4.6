772fb5031236de5d41cf2fd9e6959bfca0ec3ec5
Voligi Arunkumar <voligiarunkumar9912345@gmail.com>
2022-09-05 21:55:06 +0530

    Add project files.

---
+++ HelloWorld/HelloWorldForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace HelloWorld
+{
+    partial class HelloWorldForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(472, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MessageLabel";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+           
+            // 
+            // HelloWorldForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1788, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "HelloWorldForm";
+            this.Text = "HelloWorldForm";
+            this.Load += new System.EventHandler(this.HelloWorldForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
