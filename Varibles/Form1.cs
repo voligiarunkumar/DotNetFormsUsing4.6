5e996a16245cdace7fb8a14b67ee56125778d2ba
Voligi Arunkumar <voligiarunkumar9912345@gmail.com>
2022-09-05 23:24:33 +0530

    added new additional error labels

--- Varibles/Form1.cs
+++ Varibles/Form1.cs
@@ -12,9 +12,60 @@ namespace Varibles
 {
     public partial class Form1 : Form
     {
+        #region varibles  
+        string[] nameArray = { "", "", "", "", };
+        int arrcount=0;
+        #endregion
+
+        #region init & Load
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (arrcount < nameArray.Length)
+            {
+                if (textBox.Text != null && textBox.Text.Trim() != "")
+                {
+                    nameArray[arrcount] = textBox.Text;
+                    arrcount++;
+                    textBox1.Text = arrcount.ToString();
+                }
+                else
+                {
+                    errormessage.Text = "name should not be null and no spaces";
+                }
+            }
+            else
+            {
+                
+                for (int i = 0; i < nameArray.Length; i++)
+                {
+                    errormessage.Text = nameArray[i];
+                }
+            }
+        }
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            errormessage.Text = "";
+     
+            textBox2.Text =nameArray.Length.ToString();
+           
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
